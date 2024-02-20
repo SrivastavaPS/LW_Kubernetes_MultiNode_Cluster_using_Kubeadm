dc0545fd367a582a4e67718bf9dbcf0f374e5ea8
SrivastavaPS <88227311+SrivastavaPS@users.noreply.github.com>
2024-02-20 21:40:27 +0530

    Update common-setup-master-slave.sh

--- common-setup-master-slave.sh
+++ common-setup-master-slave.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
 
-swapoff -a
-dnf install -y iproute-tc
+swapoff -a  # optional but its good to do as it may impact performance
+dnf install -y iproute-tc  #install the traffic control utility package
 
-modprobe overlay
-modprobe br_netfilter
+modprobe overlay # it will load the driver overlay
+modprobe br_netfilter # it will load the driver br_netfilter (used for managing patting)
 
+# to make loading of drivers permanent, post reboot also, it will be loaded
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
 
+# to enable IP forwarding post reboot also, it will be enabled using this
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
-sysctl --system
+sysctl --system  # to activate above modules
 
-setenforce 0
-sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+setenforce 0  # stopping seLinux
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config # permanently stopping seLinux
 
-KUBERNETES_VERSION=v1.29
-PROJECT_PATH=prerelease:/main
+KUBERNETES_VERSION=v1.29       #Command line variable for k8s
+PROJECT_PATH=prerelease:/main  #Command line variable for cri-o
 
-cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/rpm/
-enabled=1
-gpgcheck=1
-gpgkey=https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/rpm/repodata/repomd.xml.key
-EOF
-
+# configure yum for cri-o
 cat <<EOF | tee /etc/yum.repos.d/cri-o.repo
 [cri-o]
 name=CRI-O
@@ -43,11 +37,23 @@ gpgcheck=1
 gpgkey=https://pkgs.k8s.io/addons:/cri-o:/$PROJECT_PATH/rpm/repodata/repomd.xml.key
 EOF
 
+dnf install -y cri-o # install cri-o
+systemctl enable --now crio #start and enable crio service
 
-dnf install -y cri-o kubelet kubeadm kubectl
+# configure yum for kubernetes
+cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/rpm/repodata/repomd.xml.key
+EOF
 
-systemctl enable --now crio
+dnf install -y kubelet kubeadm kubectl #install kubelet, kubeadm, kubectl
 
-systemctl enable --now kubelet
+systemctl enable --now kubelet #start and enable the kubelet service
 
+#crictl can be used just like kubectl
 
+*****************************
